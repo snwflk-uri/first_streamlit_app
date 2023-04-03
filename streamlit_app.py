d40fc1967289fbc9f692edd5732dbbef92b35f2f
snwflk-uri <129495067+snwflk-uri@users.noreply.github.com>
2023-04-03 12:02:22 +0200

    Update streamlit_app.py
    imported requests and added fruity response for watermelon

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 import pandas
+import requests
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -20,3 +22,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
